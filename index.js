c7e3b3b216f7973df97d3cd0977bbbd0d02272e8
BZIvanov <ivanov.biser@gmx.com>
2020-06-01 23:05:59 +0300

    added english and belgium language colors

--- index.js
+++ index.js
@@ -1,3 +1,142 @@
-function hello(text) {
-  return text + '!';
+const englishColors = [
+  'antra blue',
+  'anthracite grey',
+  'anthracite blue',
+  'ark blue',
+  'aura glow',
+  'aura black',
+  'breathing crystal',
+  'carbon grey',
+  'charcoal grey',
+  'cloud pink',
+  'cosmic black',
+  'crystal pink',
+  'cyan green',
+  'dark blue',
+  'denim grey',
+  'glacier blue',
+  'midnight black',
+  'midnight green',
+  'misty lavender',
+  'prism black',
+  'prism white',
+  'rose gold',
+  'space blue',
+  'space grey',
+  'steel black',
+  'anthracite',
+  'black',
+  'blue',
+  'brown',
+  'charcoal',
+  'crystal',
+  'coral',
+  'gold',
+  'graphite',
+  'green',
+  'grey',
+  'gunmetal',
+  'ice',
+  'iron',
+  'pink',
+  'red',
+  'orange',
+  'oxygen',
+  'purple',
+  'sand',
+  'silver',
+  'spacegrey',
+  'steel',
+  'white',
+  'wit',
+  'yellow',
+];
+
+const belgiumColors = [
+  'antra blue',
+  'anthracite grey',
+  'anthracite blue',
+  'ark blue',
+  'aura glow',
+  'aura black',
+  'breathing crystal',
+  'carbon grey',
+  'charcoal grey',
+  'cloud pink',
+  'cosmic black',
+  'crystal pink',
+  'cyan green',
+  'dark blue',
+  'denim grey',
+  'glacier blue',
+  'midnight black',
+  'midnight green',
+  'misty lavender',
+  'prism black',
+  'prism white',
+  'rose gold',
+  'rosé goud',
+  'space blue',
+  'space grey',
+  'steel black',
+  'anthracite',
+  'black',
+  'blue',
+  'brown',
+  'charcoal',
+  'crystal',
+  'coral',
+  'gold',
+  'goud',
+  'graphite',
+  'green',
+  'grey',
+  'gunmetal',
+  'ice',
+  'iron',
+  'koraal',
+  'pink',
+  'red',
+  'roségoud',
+  'rozegoud',
+  'orange',
+  'oxygen',
+  'purple',
+  'sand',
+  'silver',
+  'spacegrey',
+  'steel',
+  'white',
+  'wit',
+  'yellow',
+  'zwart',
+];
+
+const text = 'APPLE Refurbished iPhone SE 16GB Roségoud+';
+
+function selectColorsLanguage(language) {
+  switch (language) {
+    case 'en':
+      return englishColors;
+    case 'be':
+      return belgiumColors;
+    default:
+      return englishColors;
+  }
 }
+
+function extractColor(text, language = 'en') {
+  const chosenColors = selectColorsLanguage(language);
+
+  for (const color of chosenColors) {
+    const re = new RegExp(`(?<=\\s|^)${color}(?=\\s|$|\\+)`, 'i');
+
+    const match = text.match(re);
+    if (match) {
+      return match[0];
+    }
+  }
+  return '';
+}
+
+console.log(extractColor(text));
